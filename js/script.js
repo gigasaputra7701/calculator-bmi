d23dd49dda63680f633fffb4aa40d5f3d5ecc480
gigasaputra7701 <gigasaputra7701@gmail.com>
2023-11-08 15:05:00 +0700

    Progress 6

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
+// Bug Output Infinity dan NaN
+//function hitung BMI
 function hitungBmi(){
-    document.getElementById("container-hasil").style.display = "block";
+    
     let pBmi = document.querySelector('.hasil-bmi');
     let phasilBmi = document.querySelector('.desc-hasil');
     let pdescBmi = document.querySelector('.desc-bmi');
@@ -8,74 +10,81 @@ function hitungBmi(){
     let listPenyakit = [];
 
     let hasilBmi = '';
-    let weight = document.getElementById('weight').value;
+    let weight = document.getElementById('weight').value; 
     let age = document.getElementById('age').value;
     let height = document.getElementById('height').value;
     let list = document.getElementById("list");
+    list.innerHTML = "";   
 
-    let bmi = weight/(Math.pow(height, 2));
-    pBmi.textContent = `${bmi.toFixed(2)}`; 
-    list.innerHTML = "";
-   
-    document.getElementById("container-penyakit").style.display = "block"; 
-
-    if(bmi < 18.5){
-        hasilBmi = 'kurang';
-        phasilBmi.textContent = `${hasilBmi}`;
-        pdescBmi.textContent = `Anda berada dalam kategori underweight atau berat badan kekurangan.
-        Cara terbaik untuk menaikkan berat badan adalah dengan menambah kalori makanan yang dikonsumsi dan berolahraga.
-        Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk menaikkan berat badan hingga batas normal.`;
-        ppenyakit.textContent = `Beberapa penyakit yang berasal dari kekurusan`;
-
-        listPenyakit = ["Osteoporosis","Gangguan mulut,gigi dan kulit","Mudah Lelah","Anemia"];
-
-        for (i = 0; i < listPenyakit.length; i++) {
-            let li = document.createElement('li');
-            li.innerText = listPenyakit[i];
-            list.appendChild (li);
+    if(age>1){
+       
+        document.getElementById("container-hasil").style.display = "block"; 
+        let bmi = weight/(Math.pow(height, 2));
+        pBmi.textContent = `${bmi.toFixed(2)}`; 
+        console.log(bmi);
+        
+        if(bmi < 18.5){
+            hasilBmi = 'kurang';
+            phasilBmi.textContent = `${hasilBmi}`;
+            pdescBmi.textContent = `Anda berada dalam kategori underweight atau berat badan kekurangan.
+            Cara terbaik untuk menaikkan berat badan adalah dengan menambah kalori makanan yang dikonsumsi dan berolahraga.
+            Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk menaikkan berat badan hingga batas normal.`;
+            ppenyakit.textContent = `Beberapa penyakit yang berasal dari kekurusan`;
+            document.getElementById("container-penyakit").style.display = "block"; 
+            listPenyakit = ["Osteoporosis","Gangguan mulut,gigi dan kulit","Mudah Lelah","Anemia"];
+    
+            for (i = 0; i < listPenyakit.length; i++) {
+                let li = document.createElement('li');
+                li.innerText = listPenyakit[i];
+                list.appendChild (li);
+            }
         }
-    }
-    else if(bmi>= 18.5 && bmi<=24.9){
-        hasilBmi = 'Normal(ideal)';
-        phasilBmi.textContent = `${hasilBmi}`;
-        pdescBmi.textContent = `Anda berada dalam kategori Normal(ideal).
-        Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk mempertahankan berat badan anda, dan tetap jaga pola makan dan berolahraga.`
-        document.getElementById("container-penyakit").style.display = "none"; 
-
-    }
-    else if(bmi>=25.0 && bmi<=29.9){
-        hasilBmi = 'berlebih';
-        phasilBmi.textContent = `${hasilBmi}`;
-        pdescBmi.textContent = `Anda berada dalam kategori overweight atau berat badan berlebih.
-        Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga.
-        Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal.`
-        ppenyakit.textContent = `Beberapa penyakit yang berasal dari kegemukan`;
-
-        listPenyakit = ["Diabetes","Hipertensi","Sakit Jantung","Osteoarth"];
-        for (i = 0; i < listPenyakit.length; i++) {
-            let li = document.createElement('li');
-            li.innerText = listPenyakit[i];
-            list.appendChild (li);
+        else if(bmi>= 18.5 && bmi<=24.9){
+            hasilBmi = 'Normal(ideal)';
+            phasilBmi.textContent = `${hasilBmi}`;
+            pdescBmi.textContent = `Anda berada dalam kategori Normal(ideal).
+            Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk mempertahankan berat badan anda, dan tetap jaga pola makan dan berolahraga.`
+            document.getElementById("container-penyakit").style.display = "none"; 
+    
+        }
+        else if(bmi>=25.0 && bmi<=29.9){
+            hasilBmi = 'berlebih';
+            phasilBmi.textContent = `${hasilBmi}`;
+            pdescBmi.textContent = `Anda berada dalam kategori overweight atau berat badan berlebih.
+            Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga.
+            Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal.`
+            ppenyakit.textContent = `Beberapa penyakit yang berasal dari kegemukan`;
+            document.getElementById("container-penyakit").style.display = "block"; 
+            listPenyakit = ["Diabetes","Hipertensi","Sakit Jantung","Osteoarth"];
+            for (i = 0; i < listPenyakit.length; i++) {
+                let li = document.createElement('li');
+                li.innerText = listPenyakit[i];
+                list.appendChild (li);
+            }
+        }
+        else if(bmi>=30){
+            hasilBmi = 'Kegemukan (Obesitas)';
+            phasilBmi.textContent = `${hasilBmi}`;
+            pdescBmi.textContent = `Anda berada dalam kategori Kegemukan atau Obesitas.
+            Cara terbaik untuk menurunkan berat badan adalah dengan mengatur/mengurangi kalori makanan yang dikonsumsi dan lebih giat berolahraga.
+            Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal.`
+            ppenyakit.textContent = `Beberapa penyakit yang berasal dari Obesitas`;
+            document.getElementById("container-penyakit").style.display = "block"; 
+            listPenyakit = ["Diabetes","Hipertensi","Sakit Jantung","Osteoarth"];
+    
+            for (i = 0; i < listPenyakit.length; i++) {
+                let li = document.createElement('li');
+                li.innerText = listPenyakit[i];
+                list.appendChild (li);
+            }
         }
     }
     else{
-        hasilBmi = 'Kegemukan (Obesitas)';
-        phasilBmi.textContent = `${hasilBmi}`;
-        pdescBmi.textContent = `Anda berada dalam kategori Kegemukan atau Obesitas.
-        Cara terbaik untuk menurunkan berat badan adalah dengan mengatur/mengurangi kalori makanan yang dikonsumsi dan lebih giat berolahraga.
-        Jika BMI anda berada dalam kategori ini maka Anda dianjurkan untuk menurunkan berat badan hingga batas normal.`
-        ppenyakit.textContent = `Beberapa penyakit yang berasal dari Obesitas`;
- 
-        listPenyakit = ["Diabetes","Hipertensi","Sakit Jantung","Osteoarth"];
-
-        for (i = 0; i < listPenyakit.length; i++) {
-            let li = document.createElement('li');
-            li.innerText = listPenyakit[i];
-            list.appendChild (li);
-        }
+        alert("Silahkan input data");
     }
 }
 
+//function print hasil
 window.onload = function (){
     document.getElementById("btn-print").addEventListener("click", ()=>{
         const hasil = document.getElementById('container-1');
@@ -88,4 +97,11 @@ window.onload = function (){
         };
         html2pdf(hasil, opt);
     })
+}
+
+//function reset form
+function reset(){
+    $(form).ready(function() { 
+            $("#d")[0].reset() 
+    }); 
 }
